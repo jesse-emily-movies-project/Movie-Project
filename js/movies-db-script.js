d5bfa414fd9043f3fd005f77edff20a975eec973
Jesse <Jdorman1289@gmail.com>
2023-07-24 16:11:05 -0500

    add the create function for CRUD

--- js/movies-db-script.js
+++ js/movies-db-script.js
@@ -4,7 +4,7 @@ const getAllMovies = async () => {
         const options = {
             method: 'GET',
             headers: {
-                'application-type': 'json',
+                'Content-Type': 'application/json',
             }
         }
 
@@ -15,18 +15,39 @@ const getAllMovies = async () => {
     }
 }
 
+const addMovie = async (movieObject) => {
+    try {
+        console.log(movieObject);
+        let url = `http://localhost:3000/movies`;
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieObject)
+        }
+
+        const response = await fetch(url, options);
+        return await response.json();
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
 
 (async () => {
 
-    let allMovies = await getAllMovies();
-    console.log(allMovies);
 
+    // let allMovies = await getAllMovies();
 
+    const testMovie = {
+        title: "Harry Potter",
+        genre: "Crime",
+        rating: 5000
+    }
 
-
-
-
-
+    await addMovie(testMovie);
 
 
 })()
